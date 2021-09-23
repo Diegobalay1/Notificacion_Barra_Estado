6d7d8a79930627cc83d23efbafc79560ceb6bd65
balay <dlolhd@gmail.com>
2021-09-23 08:58:16 +0200

    Commit de definición de el contenido de la notificación, con un icono, titulo y texto del cuerpo.

--- app/src/main/java/com/diego/kotlin/notificacionbarraestado/MainActivity.kt
+++ app/src/main/java/com/diego/kotlin/notificacionbarraestado/MainActivity.kt
@@ -2,10 +2,26 @@ package com.diego.kotlin.notificacionbarraestado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
+    public val CHANNEL_ID = "channel_id"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.icono_notificacion)
+                .setContentTitle("!Hola mundo!")
+                .setContentText("Mi ejemplo de notificación")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        }
+
     }
 }
