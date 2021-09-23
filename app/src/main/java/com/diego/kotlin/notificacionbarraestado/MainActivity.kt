ffecdeefb6a302adbf72f2a1d43b9381a0cf7d10
balay <dlolhd@gmail.com>
2021-09-23 17:07:35 +0200

    Commit de creación del canal de notificación. A partir de la versión 8 de Android es obligatorio registrar este canal.

--- app/src/main/java/com/diego/kotlin/notificacionbarraestado/MainActivity.kt
+++ app/src/main/java/com/diego/kotlin/notificacionbarraestado/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.diego.kotlin.notificacionbarraestado
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,13 +13,30 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+    // Identificador único que usaremos para hacer referencia a este canal:
     public val CHANNEL_ID = "channel_id"
+
+    private fun createNotificationChannel() {
+        // Crear el canal solo para API 26+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = getString(R.string.channel_description)
+
+            // Registramos el canal en el sistema
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.button)
+
+        //createNotificationChannel()
 
         button.setOnClickListener {
             val builder = NotificationCompat.Builder(this, CHANNEL_ID)
@@ -29,9 +50,13 @@ class MainActivity : AppCompatActivity() {
                                                                 PendingIntent.FLAG_UPDATE_CURRENT)
             builder.setContentIntent(pendingIntent)
 
+            //createNotificationChannel()
+
             val NOTIF_ID = 1
             val notificationManager = NotificationManagerCompat.from(this@MainActivity)
             notificationManager.notify(NOTIF_ID, builder.build())
+
+            createNotificationChannel()
 
         }
 
